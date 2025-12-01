1ba13f00158147cb2f7fff238daa910abad4ae2d
PieFlavr <lucasadampinto@gmail.com>
2025-12-01 11:49:22 -0800

    Refactored old player scripts + fixed bugs.
     Refactored existing scripts to work with above. [DONE]
    Refactored Player, PlayerController, and PlayerManager to register under the EntityManager. [DONE]
    Fixed PlayerController not subscribing to events and handling movement cancellation. [DONE]
    Fixed InputManager.Instance.UpdateModeMouseLookDirection() to be based on main camera position instead of finnicky InputManager transform. [DONE]
    Removed InputManager from player. [DONE]
    Added cancellation logic to InputManager for OnMove. [DONE]
    Gave PlayerManager the ability to find the PlayerEntity on sceneload. [DONE]
    Refactored PlayerController to use StatsComponent for player movement speed. [DONE]
    [!!!] All of above was quite ugly under time constraint -- should be reviewed and cleaned up. [!!!]

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance {get; private set;}
-    [SerializeField] private Player player;
+    [SerializeField] private PlayerEntity player;
     private bool playerDead = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -18,22 +18,48 @@ public class PlayerManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        // Ensure we detect player instances when scenes load
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Try to find a PlayerEntity in the newly loaded scene (if any)
+        if (player == null)
+        {
+            var found = FindAnyObjectByType<PlayerEntity>();
+            if (found != null)
+            {
+                player = found;
+                Debug.Log($"[PlayerManager] Assigned PlayerEntity from scene: {found.name}");
+
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(SceneManager.GetActiveScene().name == "Level1" && this.player.GetHealth() <= 0) 
+        if (SceneManager.GetActiveScene().name == "Level1" && player != null)
         {
-            if(!this.playerDead) 
+            if (player.Stats != null && player.Stats.IsDead())
             {
-                this.playerDead = true;
-                SceneManager.LoadScene("StartScene");
-            }
-            else 
-            {
-                this.playerDead = false;
-                this.player.ResetHealth();
+                Debug.Log("[PlayerManager] Player has died, reloading start scene.");
+                if (!this.playerDead)
+                {
+                    this.playerDead = true;
+                    SceneManager.LoadScene("StartScene");
+                }
+                else
+                {
+                    this.playerDead = false;
+                    player.Stats.ResetHealth();
+                }
             }
         }
     }
