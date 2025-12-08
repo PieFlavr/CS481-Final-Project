d4c31d713c8e4a995a54539f9d8fbb8c27871882
Marlo Ongkingco <moon.ongkingco@gmail.com>
2025-12-08 13:30:26 -0800

    Show win/lose screen

--- Assets/Scripts/UI/UIPanels/EndPanel.cs
+++ Assets/Scripts/UI/UIPanels/EndPanel.cs
@@ -1,14 +1,51 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class EndPanel : UIPanel
 {
     [SerializeField] private string menuSceneName;
+    [SerializeField] private TMP_Text endMessageText;
+    [SerializeField] private string defeatMessage = "You lost!";
+    [SerializeField] private string victoryMessage = "You won!";
+    bool won;
 
     public override void Initialize()
     {
         base.Initialize();
-        PlayerManager.PlayerDied += () => this.Show();
+        PlayerManager.PlayerDied += OnPlayerDied;
+        PlayerManager.PlayerWon += OnPlayerWon;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerManager.PlayerDied -= OnPlayerDied;
+        PlayerManager.PlayerWon -= OnPlayerWon;
+    }
+
+    private void OnPlayerDied()
+    {
+        won = false;
+        ShowWithMessage(defeatMessage);
+    }
+
+    private void OnPlayerWon()
+    {
+        won = true;
+        ShowWithMessage(victoryMessage);
+    }
+
+    private void ShowWithMessage(string message)
+    {
+        if (endMessageText != null)
+        {
+            endMessageText.text = message;
+        }
+        else
+        {
+            Debug.LogWarning("[EndPanel] End message text is not assigned.");
+        }
+        Show();
     }
 
     public void QuitToMenu()
@@ -17,6 +54,9 @@ public class EndPanel : UIPanel
         Time.timeScale = 1f;
 
         // Load the main menu scene
-        SceneManager.LoadScene(menuSceneName);
+        if (!won)
+            SceneManager.LoadScene(menuSceneName);
+
+        Hide();
     }
 }
